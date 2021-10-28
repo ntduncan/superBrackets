d5a72417cf63260c0023cfc8563ae08af94c5374
ntduncan <nateduncan0@.com>
2021-10-27 18:30:58 -0600

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+
+const app = express();
+const csrfProtection = csrf();
+const PORT = process.env.PORT || 5000;
+
+app
+  .use(express.static(__dirname + "/public")) //static files
+  .set("view engine", "ejs");
+
+
+app.get("/", (req, res, next) => {
+    res.render("index");
+  })
+
+app.listen(PORT, () => {
+  console.log("Server connected at:", PORT);
+});
