570f6376ef8bb2d164a465b17c2a4d6fc4eed504
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2021-11-02 10:32:32 -0600

    added user

--- models/Bracket.js
+++ models/Bracket.js
@@ -3,19 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-   // userId: {
-   //    type: Schema.Types.ObjectId,
-   //    ref: "Bracket",
-   //    required: true
-   // }
-   // leftLevels: {
-   //    type: Number,
-   //    required: true
-   // }
-   // rightLevels: {
-   //    type: Number,
-   //    required: true
-   // }
+   userId: {
+      type: Schema.Types.ObjectId,
+      ref: "Bracket",
+      required: true,
+   },
 });
 
 module.exports = mongoose.model("Product", productSchema);
