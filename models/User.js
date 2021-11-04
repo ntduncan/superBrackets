bf5641f3df497d7e59809bb6292460f56c1d0677
ntduncan <nateduncan0@.com>
2021-11-04 10:11:34 -0600

    updated user exports

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-
 const userSchema = new Schema({
    email: {
       type: String,
@@ -25,4 +24,5 @@ const userSchema = new Schema({
    },
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
