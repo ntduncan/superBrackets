a88267249f28f78c44bc503f99b46e4026aa1262
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2021-11-04 10:18:10 -0600

    updated package.json

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const express = require("express");
 const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const router = express.Router();
 
