38c69eba569abfabf610806cc8a8195333a23c42
Braden Hansen <71408639+bradenkh@users.noreply.github.com>
2021-11-11 10:32:14 -0700

    Cors error should be fixed now

--- app.js
+++ app.js
@@ -41,10 +41,7 @@ app.use(function (req, res, next) {
    );
 
    // Request headers you wish to allow
-   res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-   );
+   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
    // Set to true if you need the website to include cookies in the requests sent
    // to the API (e.g. in case you use sessions)
