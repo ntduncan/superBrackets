ab38de86dd56416889f314b0b4ed13c2d518f694
ntduncan <nateduncan0@.com>
2021-11-30 11:11:04 -0700

    all the stuff for brackets

--- controllers/brackets.js
+++ controllers/brackets.js
@@ -14,11 +14,12 @@ const User = require("../models/User");
  * get all  brackets
  *    get all the brackets
  ***/
-exports.getAll = (req, res, next) => { // FINISHED
-   Bracket.find().then((bracket) => {
-      let jsonData = bracket;
-      res.json(jsonData);
-   });
+exports.getAll = (req, res, next) => {
+  // FINISHED
+  Bracket.find().then((bracket) => {
+    let jsonData = bracket;
+    res.json(jsonData);
+  });
 };
 
 /***
@@ -27,15 +28,15 @@ exports.getAll = (req, res, next) => { // FINISHED
  *    user's id and render them on a page
  ***/
 exports.getUserBrackets = (req, res, next) => {
-   Bracket.find({ userId: req.body.userId }) // this needs changed to however we choose to identify users
-      .then((brackets) => {
-         res.json(brackets);
-      })
-      .catch((err) => {
-         const error = new Error(err);
-         error.httpStatusCode = 500;
-         return next(error);
-      });
+  Bracket.find({ userId: req.body.userId }) // this needs changed to however we choose to identify users
+    .then((brackets) => {
+      res.json(brackets);
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 /***
@@ -45,48 +46,86 @@ exports.getUserBrackets = (req, res, next) => {
  *    and modify it so participants lose/win rounds
  ***/
 exports.getOneBracket = (req, res, next) => {
-   const bracketId = req.body.bracketId;
-   Bracket.findById(bracketId)
-      .then((bracket) => {
-         res.json(bracket);
-      })
-      .catch((err) => {
-         const error = new Error(err);
-         error.httpStatusCode = 500;
-         return next(error);
-      });
+  const bracketId = req.params.bracketId;
+  console.log(bracketId);
+  Bracket.findById(bracketId)
+    .then((bracket) => {
+      res.json(bracket);
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
-exports.editBracket = (req, res, next) => {};
+exports.editBracket = (req, res, next) => {
+  // if(req.session.isLoggedIn){
+  const id = req.params.bracketId;
+
+  const data = req.body;
+  console.log(data);
+  const bracket = {
+    title: data.title,
+    description: data.description,
+    participants: data.participants,
+  };
+
+  Bracket.findByIdAndUpdate(id, bracket)
+    .then(() => res.sendStatus(200))
+    .catch((err) => {
+      console.log(err);
+      res.send(err.statusCode, err.message);
+    });
+  // } else {
+  //     res.redirect('/products');
+  // }
+};
+
+exports.advanceParticipant = (req, res, next) => {
+  const bracketId = req.params.bracketId;
+  const participantInQuestionId = req.params.pariticipantId;
+  Bracket.findById(bracketId).then((data) => {
+    return data;
+  })
+   .then((bracket) => {
+     bracket.participants.forEach((participant) => {
+       if (participant._id === participantInQuestionId) {
+         participant.rounds++;
+       }
+       res.sendStatus(200);
+     });
+   });
+};
 
 exports.postAddBracket = (req, res, next) => {
-   const title = req.headers.title;
-   const description = req.headers.description;
-   const participants = req.headers.participants.split("%"); // should be sent as a list of names separated by commas
-   
-   let participantsWithRounds = [];
-   participants.forEach((person) => {
-      let tempPersonObj = {
-         name: person,
-         round: 1,
-      };
-      participantsWithRounds.push(tempPersonObj);
-   });
+  const title = req.headers.title;
+  const description = req.headers.description;
+  const participants = req.headers.participants.split("%"); // should be sent as a list of names separated by commas
 
-   const bracket = new Bracket({
-      title: title,
-      description: description,
-      participants: participantsWithRounds,
-   });
-   bracket
-      .save()
-      .then((result) => {
-         console.log("CREATED BRACKET");
-         res.json({ message: "Bracket was saved" });
-      })
-      .catch((err) => {
-         console.log(err); //may want to update this later
-      });
+  let participantsWithRounds = [];
+  participants.forEach((person) => {
+    let tempPersonObj = {
+      name: person,
+      round: 1,
+    };
+    participantsWithRounds.push(tempPersonObj);
+  });
+
+  const bracket = new Bracket({
+    title: title,
+    description: description,
+    participants: participantsWithRounds,
+  });
+  bracket
+    .save()
+    .then((result) => {
+      console.log("CREATED BRACKET");
+      res.json({ message: "Bracket was saved" });
+    })
+    .catch((err) => {
+      console.log(err); //may want to update this later
+    });
 };
 
 /***
@@ -95,22 +134,22 @@ exports.postAddBracket = (req, res, next) => {
  ***/
 // this says it is working but it is not
 exports.postDeleteBracket = (req, res, next) => {
-   const bracketId = req.headers.bracketid;
-   console.log(bracketId);
-   deleteQuery = { _id: bracketId };
-   Bracket.findById(bracketId).then((doc) => {
-      if (doc == null) {
-         res.json({ message: "Bracket id was invalid" });
-      }
-   });
-   Bracket.deleteOne(deleteQuery, function (err, obj) {
-      if (err) throw err;
-   });
-   Bracket.findById(bracketId).then((doc) => {
-      if (doc == null) {
-         res.json({ message: "Bracket was deleted" });
-      } else {
-         res.json({ message: "Bracket was not delted" });
-      }
-   });
+  const bracketId = req.headers.bracketid;
+  console.log(bracketId);
+  deleteQuery = { _id: bracketId };
+  Bracket.findById(bracketId).then((doc) => {
+    if (doc == null) {
+      res.json({ message: "Bracket id was invalid" });
+    }
+  });
+  Bracket.deleteOne(deleteQuery, function (err, obj) {
+    if (err) throw err;
+  });
+  Bracket.findById(bracketId).then((doc) => {
+    if (doc == null) {
+      res.json({ message: "Bracket was deleted" });
+    } else {
+      res.json({ message: "Bracket was not deleted" });
+    }
+  });
 };
--- routes/brackets.js
+++ routes/brackets.js
@@ -12,16 +12,24 @@ const router = express.Router();
 const bracketsController = require("../controllers/brackets");
 const bodyParser = require("body-parser");
 
-let urlencodedParser = bodyParser.urlencoded({extended: false})
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // getting all brackets
 router.get("/brackets-list", bracketsController.getAll);
 // getting a users brackets
-router.post("/my-brackets", urlencodedParser, bracketsController.getUserBrackets);
+router.post(
+  "/my-brackets",
+  urlencodedParser,
+  bracketsController.getUserBrackets
+);
 
 // viewing/editing a specific bracket
 router.get("/bracket/:bracketId", bracketsController.getOneBracket);
-router.post("/edit-bracket/", bracketsController.editBracket);
+router.get(
+  "/advance-participant/:bracketId/:participantId",
+  bracketsController.advanceParticipant
+);
+router.post("/edit-bracket/:bracketId", bracketsController.editBracket);
 
 // adding a bracket
 router.post("/add-bracket", bracketsController.postAddBracket);
