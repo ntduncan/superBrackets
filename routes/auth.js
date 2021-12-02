8744b5dee52ecf3335dd0c03007ce91a359039a6
Brenton Trebilcock <tre15004@byui.edu>
2021-12-02 10:34:46 -0700

    working on auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,9 @@ const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 const User = require("../models/User");
+const bodyParser = require("body-parser");
 
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 const router = express.Router();
 
 router.get("/login", authController.getLogin);
@@ -74,6 +76,7 @@ router.post(
 // testing edits
 router.post(
    "/signup",
+   urlencodedParser,
    [
       check("email")
          .isEmail()
