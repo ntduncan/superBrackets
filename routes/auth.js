069a8a8508feefec4ee980ea43b0e65dfcec02dc
Brenton Trebilcock <tre15004@byui.edu>
2021-12-02 10:30:56 -0700

    working on auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -91,8 +91,7 @@ router.post(
                }
             });
          })
-         // .normalizeEmail(),
-         ,
+         .normalizeEmail(),
       body(
          "password",
          "Please enter a password with only numbers and text and at least 5 characters."
